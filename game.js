4d5df1f04a72ab24b51701f0eac3e3ff1a5e0c8a
codesnare1 <onwuneme@guests-MacBook-Pro.local>
2015-09-30 15:11:17 -0700

    Error correction

--- game.js
+++ game.js
@@ -24,7 +24,10 @@ console.log('Question 1 response:' + question1);
 console.log('What I am testing in my "if" conditions:' + question1.toUpperCase());
 
 console.log('Question 2 response:' + question2);
+console.log('What I am testing in my "if" conditions:' + question2.toUpperCase());
+
 console.log('Question 3 response:' + question3);
+console.log('What I am testing in my "if" conditions:' + question3.toUpperCase());
 
 alert('Your answer to Question 1 was ' + question1)
 alert('Your answer to Question 2 was ' + question2)
