602c99daaeafd6c73e413dd5d3ff97cb5c333385
codesnare1 <onwuneme@guests-MacBook-Pro.local>
2015-09-30 15:02:54 -0700

    Modified user response

--- game.js
+++ game.js
@@ -1,26 +1,28 @@
 
 
 var question1 = prompt('Am I from Seattle?');
-if (question1 === no) {
+if (question1 === "NO") {
   alert ('Way to Go');
 } else {
   alert('Oops, I am from North Carolina');
 }
 
 var question2 = prompt('Do I like dogs?');
-if (question2 === yes) {
-  alert ('Way to Go')
+if (question2 === "YES") {
+  alert ('Way to Go!');
 } else {
   alert('Mamma Mia');
 }
 var question3 = prompt('Do I have brown eyes?');
-if (question3 === yes) {
-  alert('Way to Go')
+if (question3 === "YES") {
+  alert('Way to Go');
 } else {
-  alert('I think you need glasses')
+  alert('I think you need glasses');
 }
 
 console.log('Question 1 response:' + question1);
+console.log('What I am testing in my "if" conditions:' + question1.toUpperCase());
+
 console.log('Question 2 response:' + question2);
 console.log('Question 3 response:' + question3);
 
