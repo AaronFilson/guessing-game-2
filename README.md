1c3b800c9d9319b2b3f9c042c3d49768517dfdd5
codesnare1 <onwuneme@guests-MacBook-Pro.local>
2015-09-30 13:40:35 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Class-demo-29-Sept
+In class demo for B34 for using GH and writing some code
---
+++ game.js
@@ -0,0 +1,19 @@
+
+
+var question1 = prompt('Am I from Seattle?');
+if (question 1 === yes) {
+  alert ('Lucky you');
+} else {
+  alert('Sucks to be you');
+}
+
+var question2 = prompt('Do I like dogs?');
+var question3 = prompt('Do I have brown eyes?');
+
+console.log('Question 1 response:' + question1);
+console.log('Question 2 response:' + question2);
+console.log('Question 3 response:' + question3);
+
+alert('Your answer to Question 1 was ' + question1)
+alert('Your answer to Question 2 was ' + question2)
+alert('Your answer to Question 3 was ' + question3)
