8c7bbe24f4231896bbbbdba5e9ca654d33dbd738
codesnare1 <kaiduku@hotmail.com>
2015-10-01 00:10:48 -0700

    User Summary

--- game.js
+++ game.js
@@ -1,27 +1,37 @@
 
+var userName= prompt('What is your name?');
+    console.log(userName);
+    alert('Hi, '+userName + '!!! Go ahead, make my day!!!');
+
+var numbercorrect = 0;
 
 var question1 = prompt('Am I from Seattle?');
 if (question1.toUpperCase() === "NO" || question1.toUpperCase() === "N") {
+  numbercorrect++;
   alert ('Way to Go!');
-} else {
+} 
+else {
   alert('Oops, I am from North Carolina');
 }
 
 var question2 = prompt('Do I like dogs?');
 if (question2.toUpperCase() === "YES" || question2.toUpperCase() === "Y") {
+  numbercorrect++;
   alert ('Way to Go!');
 } else {
   alert('Mamma Mia');
 }
 var question3 = prompt('Do I have brown eyes?');
 if (question3.toUpperCase() === "YES" || question3.toUpperCase() === "Y") {
+  numbercorrect++
   alert('Way to Go!');
 } else {
   alert('I think you need glasses');
 }
 
+alert(userName + ' got ' + numbercorrect + ' out of 3 questions correct ');
 
-
+/*
 console.log('Question 1 response:' + question1);
 console.log('What I am testing in my "if" conditions:' + question1.toUpperCase());
 
@@ -34,3 +44,4 @@ console.log('What I am testing in my "if" conditions:' + question3.toUpperCase()
 alert('Your answer to Question 1 was ' + question1)
 alert('Your answer to Question 2 was ' + question2)
 alert('Your answer to Question 3 was ' + question3)
+*/
